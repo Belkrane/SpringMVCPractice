9a6d6206aa0a713658c7c51be86ec1da9d472428
Belkrane <jonghagi77@gmail.com>
2023-06-12 08:10:20 +0900

    InternalResourceViewResolver

--- java-servlet-demo/me.belk/src/main/java/me/belk/WebConfig.java
+++ java-servlet-demo/me.belk/src/main/java/me/belk/WebConfig.java
@@ -1,4 +1,13 @@
 package me.belk;
 
+@Configuraion
+@ComponentScan
 public class WebConfig {
+
+    @Bean
+    public InternalResourceViewResolver viewResolver(){
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/");
+        viewResolver.setSuffix(".jsp");
+    }
 }
