c1c714c45f00d8eeb6cede90b3407e3f7cc6ff25
Belkrane <jonghagi77@gmail.com>
2023-06-20 17:18:39 +0900

    Learn about Handler Interceptor

--- demo-boot-web/src/main/java/me/belk/demobootweb/SampleController.java
+++ demo-boot-web/src/main/java/me/belk/demobootweb/SampleController.java
@@ -7,6 +7,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SampleController {
+    //preHandle -> 전처리
+    //preHandle2 -> 여러개 있을 경우(순서는 오더라는 속성으로 줄 수 있음)
+    //hello
+    //요청처리
+    //postHandle2 -> 포스트와 애프터는 역순으로 호출 됨
+    //postHandle -> 후처리
+    //뷰 랜더링
+    //afterCompletion2 -> 역순으로 호출 됨
+    //afterCompletion -> 뷰 랜더링 후처리
 
     @GetMapping("/hello")
     public String hello(@RequestParam("name") Person person){
--- demo-boot-web/src/test/java/me/belk/demobootweb/SampleControllerTest.java
+++ demo-boot-web/src/test/java/me/belk/demobootweb/SampleControllerTest.java
@@ -3,7 +3,9 @@ package me.belk.demobootweb;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -13,7 +15,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest
+@SpringBootTest
+@AutoConfigureMockMvc
 public class SampleControllerTest {
 
     @Autowired
