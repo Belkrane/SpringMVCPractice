8e419d893f59eed0b46da2149464b65c0260149c
Belkrane <jonghagi77@gmail.com>
2023-06-21 23:50:28 +0900

    resource handler

--- demo-boot-web/src/test/java/me/belk/demobootweb/SampleControllerTest.java
+++ demo-boot-web/src/test/java/me/belk/demobootweb/SampleControllerTest.java
@@ -1,18 +1,20 @@
 package me.belk.demobootweb;
 
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.Assert.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -36,5 +38,23 @@ public class SampleControllerTest {
         this.mockMvc.perform(get("/hello").param("id", "1"))
                 .andDo(print())
                 .andExpect(content().string("hello belk"));
+    }
+
+    @Test
+    public void helloStatic() throws Exception {
+        this.mockMvc.perform(get("/index.html"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(Matchers.containsString("Hello index")));
+    }
+
+
+    @Test
+    public void helloMobile() throws Exception {
+        this.mockMvc.perform(get("/mobile/index.html"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(Matchers.containsString("Hello mobile")))
+                .andExpect(header().exists(HttpHeaders.CACHE_CONTROL));
     }
 }
