918645a122285f06a4f33f2fbfb3127a92be7d42
Belkrane <jonghagi77@gmail.com>
2023-06-28 23:07:53 +0900

    request content type mapping

--- demo-web-mvc/src/main/java/me/belk/demowebmvc/SampleController.java
+++ demo-web-mvc/src/main/java/me/belk/demowebmvc/SampleController.java
@@ -1,23 +1,16 @@
 package me.belk.demowebmvc;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping("/hello")
 public class SampleController {
 
-    @RequestMapping("/belk")
+    @RequestMapping(value = "/hello", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
     @ResponseBody
-    public String hellobelk(){
+    public String hello(){
         return "hello belk";
-    }
-
-
-    @RequestMapping("/{name:[a-z]+}")
-    @ResponseBody
-    public String hello(@PathVariable String name){
-        return "hello";
     }
 
     /*
@@ -26,5 +19,10 @@ public class SampleController {
       2.여러개의 문자열로 맵핑
       3.특정 패턴으로 맵핑(정규식으로 받을 수 있음), 패턴이 중복되는 경우 가장 구체적으로 맵핑되는것
       - Spring에서는 URI Pattern + ".*" 를 기본적으로 제공함, Boot에서는 제공 안함
+     */
+
+    /*
+        1. 특정한 Type의 요청만 받고자 할때
+         - consumes option에 해당 타입을 정의하면 된다.
      */
 }
--- demo-web-mvc/src/test/java/me/belk/demowebmvc/SampleControllerTest.java
+++ demo-web-mvc/src/test/java/me/belk/demowebmvc/SampleControllerTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,11 +22,9 @@ public class SampleControllerTest {
 
     @Test
     public void helloTest() throws Exception{
-        mockMvc.perform(get("/hello/belk"))
+        mockMvc.perform(get("/hello")
+                        .contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
                 .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(content().string("hello belk"))
-                .andExpect(handler().handlerType(SampleController.class))
-                .andExpect(handler().methodName("hellobelk"));
+                .andExpect(status().isOk());
     }
 }
